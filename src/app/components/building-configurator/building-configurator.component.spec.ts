ba466e751d3e950f8886bfaa21ff968daed215bb
Edu Caribe <edu.caribe@hotmail.com>
2022-02-15 08:44:34 -0300

    test: add occupant appear interval ui control

--- src/app/components/building-configurator/building-configurator.component.spec.ts
+++ src/app/components/building-configurator/building-configurator.component.spec.ts
@@ -3,23 +3,41 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BuildingConfiguratorComponent } from './building-configurator.component';
 
 describe('BuildingConfiguratorComponent', () => {
-  let component: BuildingConfiguratorComponent;
-  let fixture: ComponentFixture<BuildingConfiguratorComponent>;
+	let component: BuildingConfiguratorComponent;
+	let fixture: ComponentFixture<BuildingConfiguratorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BuildingConfiguratorComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [BuildingConfiguratorComponent]
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BuildingConfiguratorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(BuildingConfiguratorComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('#ngOnInit', () => {
+		it('should call #initForms', () => {
+
+		});
+	});
+
+	describe('#initForms', () => {
+		it('should define form group with default randomOccupantsService values', () => {
+
+		});
+	});
+
+	describe('#updateParams', () => {
+		it('should call randomOccupantsService #changeOccupantsAppearInterval with appear interval form control value', () => {
+
+		});
+	});
 });
