da893d464dc0d7c225f538b32ba292cdce5386e3
Edu Caribe <edu.caribe@hotmail.com>
2022-02-15 09:18:43 -0300

    feat: change occupant appear interval from building configurator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -22,11 +23,12 @@ import { RandomOccupantsComponent } from './components/random-occupants/random-o
 		BuildingFloorComponent,
 		ElevatorComponent,
 		ElevatorPanelComponent,
-  BuildingConfiguratorComponent,
-  RandomOccupantsComponent
+		BuildingConfiguratorComponent,
+		RandomOccupantsComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		ReactiveFormsModule
 	],
 	providers: [
 		ElevatorService,
--- src/app/components/building-configurator/building-configurator.component.ts
+++ src/app/components/building-configurator/building-configurator.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { RandomOccupantService } from 'src/app/services/random-occupant/random-occupant.service';
 
 @Component({
 	selector: 'tce-building-configurator',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
 	styleUrls: ['./building-configurator.component.scss']
 })
 export class BuildingConfiguratorComponent {
+	/* Params form group */
+	public paramsForm: FormGroup | undefined;
 
+	constructor(public randomOccupantsService: RandomOccupantService) { }
+
+	/**
+	 * ngOnInit
+	 *
+	 * Angular lifecycle method - executed after directive is initialized
+	 */
+	public ngOnInit(): void {
+		this.initForm();
+	}
+
+	/**
+	 * initForm
+	 *
+	 * Initialize form group with default service params values
+	 */
+	public initForm(): void {
+		this.paramsForm = new FormGroup({
+			occupantsAppearInterval: new FormControl(
+				this.randomOccupantsService.appearInterval$.value,
+				[Validators.required, Validators.min(1)]
+			)
+		});
+	}
+
+	/**
+	 * updateParams
+	 *
+	 * Update service dynamic params
+	 */
+	public updateParams(): void {
+		const interval: number = this.paramsForm?.value.occupantsAppearInterval || 0;
+		this.randomOccupantsService.changeOccupantsAppearInterval(interval);
+	}
 }
