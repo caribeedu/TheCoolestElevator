1e837071fce0e58a6f733f2d83e4f1e3ba344742
Edu Caribe <edu.caribe@hotmail.com>
2022-02-15 09:23:10 -0300

    test: add building service to test module providers

--- src/app/components/building/building.component.spec.ts
+++ src/app/components/building/building.component.spec.ts
@@ -1,25 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BuildingService } from 'src/app/services/building/building.service';
 
 import { BuildingComponent } from './building.component';
 
 describe('BuildingComponent', () => {
-  let component: BuildingComponent;
-  let fixture: ComponentFixture<BuildingComponent>;
+	let component: BuildingComponent;
+	let fixture: ComponentFixture<BuildingComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BuildingComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			providers: [BuildingService],
+			declarations: [BuildingComponent]
+		}).compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BuildingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(BuildingComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
