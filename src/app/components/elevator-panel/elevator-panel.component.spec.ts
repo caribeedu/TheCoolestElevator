7ec08f87810724c2186ca2fb88b0009e46cea893
Edu Caribe <edu.caribe@hotmail.com>
2022-02-16 12:17:41 -0300

    test: validate doors state and current floor on trying travel

--- src/app/components/elevator-panel/elevator-panel.component.spec.ts
+++ src/app/components/elevator-panel/elevator-panel.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ElevatorPanelComponent } from './elevator-panel.component';
 
 import { ElevatorService } from 'src/app/services/elevator/elevator.service';
+import { BuildingService } from 'src/app/services/building/building.service';
+
+import { ElevatorDoor } from 'src/app/enum/elevator.enum';
 
 describe('ElevatorPanelComponent', () => {
 	let component: ElevatorPanelComponent;
@@ -11,7 +14,10 @@ describe('ElevatorPanelComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [ElevatorPanelComponent],
-			providers: [ElevatorService]
+			providers: [
+				ElevatorService,
+				BuildingService
+			]
 		}).compileComponents();
 	});
 
@@ -26,12 +32,32 @@ describe('ElevatorPanelComponent', () => {
 	});
 
 	describe('#travel', () => {
-		it('should call elevatorService newPanelCall$ #next with desired floor', () => {
+		let newPanelCallSpy: jasmine.Spy;
 
+		beforeEach(() => {
+			newPanelCallSpy = spyOn(component.elevatorService.newPanelCall$, 'next');
+		});
+
+		it('should call elevatorService newPanelCall$ #next with desired floor', () => {
+			spyOnProperty(component.elevatorService.currentFloor$, 'value').and.returnValue(1);
+			spyOnProperty(component.elevatorService.doors$, 'value').and.returnValue(ElevatorDoor.CLOSED);
+
+			const floorDouble: number = 2;
+
+			component.travel(floorDouble);
+
+			expect(newPanelCallSpy).toHaveBeenCalledOnceWith(floorDouble);
 		});
 
 		it('shouldn\'t call elevatorService newPanelCall$ #next with desired floor if this is the current floor and doors is opened', () => {
+			const floorDouble: number = 2;
 
+			spyOnProperty(component.elevatorService.currentFloor$, 'value').and.returnValue(floorDouble);
+			spyOnProperty(component.elevatorService.doors$, 'value').and.returnValue(ElevatorDoor.OPENED);
+
+			component.travel(floorDouble);
+
+			expect(newPanelCallSpy).not.toHaveBeenCalled();
 		});
 	});
 });
